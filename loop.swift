efa4beb4cfa61058f30812c5096ac019538c322d
Mrittika Dutta <129971118+MrittikaDutta@users.noreply.github.com>
2025-08-03 10:13:06 +0530

    Create loop.swift

---
+++ loop.swift
@@ -0,0 +1,9 @@
+var fruit = "apple"
+switch fruit {
+    case "banana":
+        print("I like bananas.")
+    case "apple":
+        print("Apples are tasty.")
+    default:
+        print("All fruits are delicious.")
+}
